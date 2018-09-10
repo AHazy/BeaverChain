31547a807c134049d1c2594f1cd9fb4bc0c1d9fc
Ahazy <haydena@oregonstate.edu>
2018-09-09 17:59:23 -0700

    Added block view functionality and README

---
+++ README.txt
@@ -0,0 +1,9 @@
+~ BeaverChain by Alec Hayden ~
+The purpose of this project is to create a transparent and immutable voting platform
+for OSU students.  The platform can be altered to be used for any voting application
+as it pertains to OSU students.  As an example, the platform is setup to count Yes
+or No votes, but it can be changed for anything else such as multiple candidate votes.
+
+Currently, the program creates a Python list that will hold the vote results.  This list
+cannot be modified and each vote in the list corresponds with a single block.  The blocks
+are connected by referring to the block that came beforehand.
--- beaverchain.py
+++ beaverchain.py
@@ -17,6 +17,7 @@ for i in range(0, total_blocks):
     block_to_add = new_block(prev_block)
     beaverchain.append(block_to_add)
     prev_block = block_to_add
+    print('This blocks hash is: {}'.format(block_to_add.hash))
     if (block_to_add.vote == 'no') or (block_to_add.vote == 'No') or (block_to_add.vote == 'n') or (block_to_add.vote == 'N'):
         total_no = total_no + 1
     else:
@@ -34,3 +35,9 @@ elif total_yes > total_no:
     print('There were more "Yes" votes than "No" votes.')
 else:
     print('The vote resulted in a tie.')
+
+block_view = input('Enter the hash of the block you want to view: ')
+for i in range(0, total_blocks):
+    if beaverchain[i].hash == block_view:
+        print('Block has been found')
+        print('Block Vote: {}'.format(beaverchain[i].vote))
